df13560f7c6b29bd54de31123312243ed52de394
Soumyadeb Basu <basusoumyadeb@gmail.com>
2025-08-03 00:59:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+//import componentImg from "./assets/components.png"; //default data imported normally
+import { CORE_CONCEPTS } from "./data"; //named data imported using {}
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcepts";
+import TabButton from "./components/TabButton";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept
+              image={CORE_CONCEPTS[1].image}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcept
+              image={CORE_CONCEPTS[2].image}
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
